6765b46eda46b2105642812410db45d301ebf004
Ionel Bădișor <ionel.badisor@gmail.com>
2025-03-26 20:37:28 +0200

    Change options to match my workflow

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -114,9 +114,9 @@ vim.opt.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
-end)
+-- vim.schedule(function()
+--   vim.opt.clipboard = 'unnamedplus'
+-- end)
 
 -- Enable break indent
 vim.opt.breakindent = true
@@ -145,7 +145,20 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+---vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = {
+  tab = '▸ ',
+  eol = '↲',
+  space = '·',
+  nbsp = '␣',
+  trail = '█',
+  extends = '⟩',
+  precedes = '⟨',
+}
+
+--spell language
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
